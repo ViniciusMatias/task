448a7d000e22f5706a3e5da82c3592a072a04397
Vinicius M. Lima <45833399+ViniciusMatias@users.noreply.github.com>
2024-08-20 09:38:05 -0300

    Init project

---
+++ src/test/java/com/br/mt/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mt.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
